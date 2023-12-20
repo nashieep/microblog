0d8740358fab75fb5d4e73dea06e28176c0778a9
nashieep <nmpongnon@gmail.com>
2023-12-20 15:50:12 -0600

    created redirect js

---
+++ location.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentloaded",function(){
+document.getElementById().addEventListener("login",function(event){
+    //Prevent the default form submission
+    event.preventDefault();
+    //redirect to "posts" page
+    window.location.href="#";
+});
+});
