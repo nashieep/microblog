f1dfc383002209cfab2e591097a131b95c581b9f
nashieep <nmpongnon@gmail.com>
2023-12-30 20:41:14 -0600

    updates

--- location.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentloaded",function(){
-document.getElementById().addEventListener("login",function(event){
-    //Prevent the default form submission
-    event.preventDefault();
-    //redirect to "posts" page
-    window.location.href="#";
-});
-});
